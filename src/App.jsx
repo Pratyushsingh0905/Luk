425e2523592b37cf6138cfdaaa30e562e81d6065
Pratyush Singh <pratyushsinghji@gmail.com>
2024-09-30 20:16:07 +0530

    Changes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Chatbot from './components/Chatbot';
+
+function App() {
+
+  return (
+    <>
+    <div className="App">
+      <Chatbot />
+    </div>
+    </>
+  )
+}
+
+export default App
